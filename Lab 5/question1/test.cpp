f1a821ddc2aa8ce0966e8156633b5db587c44d03
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-06-29 00:07:35 -0700

    Update test.cpp

--- Lab 5/question1/test.cpp
+++ Lab 5/question1/test.cpp
@@ -1,9 +1,4 @@
-/* 
-Create a class Counted that contains an int id and a static int count. 
-The default constructor should begin: Counted() : id(count++). It should 
-also print its id and that it’s being created. The destructor should print 
-that it’s being destroyed and its id. Test your class.
-*/
+
 #include <iostream>
 using namespace std;
 
