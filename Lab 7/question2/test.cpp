d1067ff67de24576addb1bd9232e09fd735e8696
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 04:06:54 -0700

    Update test.cpp

--- Lab 7/question2/test.cpp
+++ Lab 7/question2/test.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 using namespace std;
 
@@ -7,20 +5,24 @@ class mySimpleClass {
 public:
     mySimpleClass(int x = 0) : x(x) {}
 
-    mySimpleClass operator+(const mySimpleClass& rhs) {
-        return mySimpleClass(x + rhs.x);
-    }
-
-    mySimpleClass operator*(const mySimpleClass& rhs) {
-        return mySimpleClass(x * rhs.x);
-    }
-
-    void print(ostream& out) {
-        out << x << endl;
-    }
+    mySimpleClass operator+(const mySimpleClass& rhs);
+    mySimpleClass operator*(const mySimpleClass& rhs);
+    void print(ostream& out);
 private:
     int x;
 };
+
+mySimpleClass mySimpleClass::operator+(const mySimpleClass& rhs) {
+    return mySimpleClass(x + rhs.x);
+}
+
+mySimpleClass mySimpleClass::operator*(const mySimpleClass& rhs) {
+    return mySimpleClass(x * rhs.x);
+}
+
+void mySimpleClass::print(ostream& out) {
+    out << x << endl;
+}
 
 int main(){
     mySimpleClass a(1), b(2), c;
