ea0e43096d608ffb97c9754405dbc36b40a6bd20
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 04:04:46 -0700

    Update test.cpp

--- Lab 5/question2/test.cpp
+++ Lab 5/question2/test.cpp
@@ -1,43 +1,43 @@
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
-// From exercise 1
 class Counted {
     int id;
     static int count;
 public:
-    Counted() : id(count++) { 
-        cout << "An object is being created is created, id: " << id << endl; 
-    }
-    ~Counted() { 
-        cout << "The created object is being destroyed, id: " << id << endl; 
-    }
-// added operator to be friend in order to print pointers of id
-friend ostream& operator<< (ostream& out, const Counted& out2);
+    Counted();
+    ~Counted();
+    friend ostream& operator<<(ostream& out, const Counted& obj);
 };
 
 int Counted::count = 0;
 
-// allows us to print pointers of id
-ostream& operator<< (ostream& out, const Counted& out2) {
-    out << out2.id;
+Counted::Counted() : id(count++) {
+    cout << "An object is being created, id: " << id << endl;
+}
+
+Counted::~Counted() {
+    cout << "The created object is being destroyed, id: " << id << endl;
+}
+
+ostream& operator<<(ostream& out, const Counted& obj) {
+    out << obj.id;
     return out;
 }
 
 int main() {
     vector<Counted*> v;
 
-    for (int i = 0; i < 10; i++) // addes to vector
+    for (int i = 0; i < 10; i++)
         v.push_back(new Counted);
 
     cout << "Vector contains: ";
-    for (int i = 0; i < v.size(); i++) // prints vector
-        cout << *v[i] << ' '; // uses our new operator access
+    for (int i = 0; i < v.size(); i++)
+        cout << *v[i] << ' ';
     cout << endl;
 
-    while (v.size() > 0) { // deletes vector
+    while (v.size() > 0) {
         Counted* temp = v.back();
         v.pop_back();
         delete temp;
