7fa7d4a267ec46b25a225c65757c53eadc8cd1be
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 04:06:20 -0700

    Update test.cpp

--- Lab 7/question1/test.cpp
+++ Lab 7/question1/test.cpp
@@ -1,23 +1,27 @@
-
 #include <iostream>
 #include <cstring>
 using namespace std;
 
 class MyClass {
 public:
-    MyClass(char c = ' ') {
-        memset(myArray, c, 100);
-    }
+    MyClass(char c = ' ');
 
-    void print(char c) {
-        for (int i = 0; i < 100; i++) {
-            myArray[i] = c;
-        }
-        cout << myArray << endl;
-    }
+    void print(char c);
+
 private:
     char myArray[100];
 };
+
+MyClass::MyClass(char c) {
+    memset(myArray, c, 100);
+}
+
+void MyClass::print(char c) {
+    for (int i = 0; i < 100; i++) {
+        myArray[i] = c;
+    }
+    cout << myArray << endl;
+}
 
 int main(){
     MyClass myObj;
