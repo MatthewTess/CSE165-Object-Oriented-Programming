73523ac5cdd320daf4b8f60c5d2b99c4f1ac8d81
Matthew <95320458+MatthewTess@users.noreply.github.com>
2023-07-01 04:05:32 -0700

    Update test.cpp

--- Lab 5/question3/test.cpp
+++ Lab 5/question3/test.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,25 +5,39 @@ using namespace std;
 class Pet {
     string pname;
 public:
-    Pet(const string& petName) : pname(petName) {}
+    Pet(const string& petName);
     virtual string name() = 0;
     virtual string speak() = 0;
-    virtual ~Pet(){}; // virtual destructor 
+    virtual ~Pet();
 };
+
+Pet::Pet(const string& petName) : pname(petName) {}
 
 string Pet::name() { return pname; }
 
+Pet::~Pet() {}
+
 class Dog : public Pet {
 public:
-    Dog(const string& petName) : Pet(petName) {}
-    virtual string sit() { return Pet::name() + " sits"; }
-    string speak() { return Pet::name() + " says 'Bark!'"; }
-    string name() { return Pet::name(); }
-    ~Dog(){}; // destructor
+    Dog(const string& petName);
+    virtual string sit();
+    string speak();
+    string name();
+    ~Dog();
 };
 
+Dog::Dog(const string& petName) : Pet(petName) {}
+
+string Dog::sit() { return Pet::name() + " sits"; }
+
+string Dog::speak() { return Pet::name() + " says 'Bark!'"; }
+
+string Dog::name() { return Pet::name(); }
+
+Dog::~Dog() {}
+
 int main() {
-    Pet* p = new Dog("Carl"); // this is my dogs name btw
+    Pet* p = new Dog("Carl");
     cout << p->speak();
     delete p;
 
